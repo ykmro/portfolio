2d7cf43165463bdc58f546e7f0f121536df016c3
ykmro <ko.kansya@gmail.com>
2026-04-08 19:50:24 +0900

    選択肢クリック、次のSTEPのロジック実装

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,8 +1,21 @@
 class StepsController < ApplicationController
   def show
+    @step = Step.find(params[:id])
+    @choices = @step.choices
   end
 
   def next
+    @step = Step.find(params[:id])
+    @next_step = @step.next_step
+
+    session[:choice_ids] ||= []
+    session[:choice_ids] << params[:choice_id]
+
+    if @next_step
+      redirect_to @next_step
+    else
+      redirect_to result_situation_path(id: @step.situation_id)
+    end
   end
 
   def result
--- app/models/step.rb
+++ app/models/step.rb
@@ -4,4 +4,8 @@ class Step < ApplicationRecord
 
   validates :step_number, presence: true
   validates :body,        presence: true
+
+  def next_step
+    Step.where(situation_id: self.situation_id, step_number: self.step_number + 1).first
+  end
 end
