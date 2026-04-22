72eea43f370b742070cac3cfa02f530f8fcc41cd
ykmro <ko.kansya@gmail.com>
2026-04-22 18:16:03 +0900

    デプロイ設定修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,8 @@ Rails.application.configure do
   # config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts << "portfolio-4vox.onrender.com"
+  # config.hosts << "portfolio-4vox.onrender.com"
+  config.hosts << "whatif-english.com"
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
