98bc4a93324e0601a8fa34d732e13a64b086d252
ykmro <ko.kansya@gmail.com>
2026-04-23 19:56:55 +0900

    メンテナンス画面追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  # allow_browser versions: :modern
+  before_action :maintenance_mode
+
+  private
+
+  def maintenance_mode
+    render "maintenance", layout: false
+  end
 end
