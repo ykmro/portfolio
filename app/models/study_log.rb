5681d367902a9832fcf7e8412a5f08fd24029455
ykmro <ko.kansya@gmail.com>
2026-06-07 17:49:11 +0900

    正答数→正答率に変更

--- app/models/study_log.rb
+++ app/models/study_log.rb
@@ -1,4 +1,10 @@
 class StudyLog < ApplicationRecord
   belongs_to :user
   belongs_to :loggable, polymorphic: true
+
+  def correct_rate
+    total = loggable.steps.length
+    return 0 if total.zero?
+    (correct_count.to_f / total * 100).floor
+  end
 end
